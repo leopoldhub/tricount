8c3ed4e3ed9c2951aca163c030aeb1dba435c2cb
leopold.hubert.etu <leopold.hubert.etu@univ-lille.fr>
2022-03-25 11:04:10 +0100

    correct error redirection

--- src/main/java/fr/univlille/da2i/hubert/etu/tricount/controller/ExceptionController.java
+++ src/main/java/fr/univlille/da2i/hubert/etu/tricount/controller/ExceptionController.java
@@ -1,45 +1,26 @@
 package fr.univlille.da2i.hubert.etu.tricount.controller;
 
 import fr.univlille.da2i.hubert.etu.tricount.utils.UrlUtils;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.StringJoiner;
 
 @ControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler({Exception.class, RuntimeException.class})
-    @RequestMapping
-    public String exceptionHandler(final Exception exception) throws Exception {
-        throw exception;
+    public String exceptionHandler(final Exception exception, final HttpServletRequest request) {
+        String message = exception.getMessage() == null ? "Unexpected exception occured!" : exception.getMessage();
+        return UrlUtils.buildRedirectUrlWithError(request.getHeader("Referer"), message);
     }
 
-    /*@ExceptionHandler({BindException.class, BadRequestException.class, ForbiddenException.class, AuthenticationCredentialsNotFoundException.class})
-    public String referExceptions(final BindException exception, final HttpServletRequest request) {
-        return UrlUtils.buildRedirectUrl(request.getHeader("Referer"), bindExceptionToString(exception));
-    }
-
-    @ExceptionHandler(UnauthorizedException.class)
-    public String unauthorizedException(final UnauthorizedException exception) {
-        return UrlUtils.buildRedirectUrl("/login", exception.getMessage());
-    }
-
-    @ExceptionHandler(Exception.class)
-    public String unexpectedException(final Exception exception, final HttpServletResponse servletResponse, final HttpServletRequest request) {
-        String response;
-
-        switch (HttpStatus.valueOf(servletResponse.getStatus())) {
-            case FORBIDDEN:
-                response = UrlUtils.buildRedirectUrl(request.getHeader("Referer"), exception.getMessage());
-                break;
-            case UNAUTHORIZED:
-                response = UrlUtils.buildRedirectUrl("/login", exception.getMessage());
-                break;
-            default:
-                response = UrlUtils.buildRedirectUrl("/unexpected", exception.getMessage());
-                break;
-        }
-        return response;
+    @ExceptionHandler(BindException.class)
+    public String bindExceptionHandler(final BindException exception, final HttpServletRequest request) {
+        return UrlUtils.buildRedirectUrlWithError(request.getHeader("Referer"), bindExceptionToString(exception));
     }
 
     public static String bindExceptionToString(final BindException exception) {
@@ -47,6 +28,6 @@ public class ExceptionController {
         exception.getBindingResult().getAllErrors().forEach((error) ->
                 sj.add(((FieldError) error).getField() + ": " + error.getDefaultMessage()));
         return sj.toString();
-    }*/
+    }
 
 }
